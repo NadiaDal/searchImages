44758cbf9c53c493c7a1ce218cd9b9634abe21be
Nadia.Daliavska <nadiia.daliavska@adraba.io>
2018-04-10 12:53:55 +0300

    add redux-logger middleware

--- App/Redux/CreateStore.js
+++ App/Redux/CreateStore.js
@@ -2,6 +2,8 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import Config from '../Config/DebugConfig'
 import createSagaMiddleware from 'redux-saga'
 import ScreenTracking from './ScreenTrackingMiddleware'
+import { createLogger } from 'redux-logger'
+import R from 'ramda'
 
 // creates the store
 export default (rootReducer, rootSaga) => {
@@ -19,6 +21,20 @@ export default (rootReducer, rootSaga) => {
   const sagaMiddleware = createSagaMiddleware({ sagaMonitor })
   middleware.push(sagaMiddleware)
 
+  /* ------------- Logger Middleware ------------- */
+
+  // remove common noise
+  const loggingBlacklist = ['EFFECT_TRIGGERED', 'EFFECT_RESOLVED', 'EFFECT_REJECTED', 'persist/REHYDRATE']
+  if (__DEV__) {
+    // the logger master switch
+    const USE_LOGGING = Config.reduxLogging
+    // silence these saga-based messages
+    // create the logger
+    const logger = createLogger({
+      predicate: (getState, { type }) => USE_LOGGING && R.not(R.contains(type, loggingBlacklist))
+    })
+    middleware.push(logger)
+  }
   /* ------------- Assemble Middleware ------------- */
 
   enhancers.push(applyMiddleware(...middleware))
